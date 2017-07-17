5db60e1a71d84ca7cf6e49969bbdad89d4027623
Robin C Samuel <robincharummoottil@gmail.com>
2017-07-17 20:07:43 +0530

    Update index.js
    @lalahehe suggestions

--- index.js
+++ index.js
@@ -4,18 +4,23 @@
  * @author : Robin C Samuel <hi@robinz.in> http://robinz.in
  * @date : 11th August 2015
  */
-var Promise     =   require('bluebird'),
-    fs          =   require('fs'),
-    googleapis  =   require('googleapis'),
-    https        =   require('https');
+var Promise = require('bluebird'),
+  fs = require('fs'),
+  googleapis = require('googleapis'),
+  https = require('https'),
+  BaseStore = require('../../../core/server/storage/base'),
+  util = require('util');
 
-function ghostGoogleDrive(config){
+function ghostGoogleDrive(config) {
+  BaseStore.call(this);
   this.config = config || {};
 };
 
-ghostGoogleDrive.prototype.save = function(file){
+util.inherits(ghostGoogleDrive, BaseStore);
+
+ghostGoogleDrive.prototype.save = function(file) {
   var _this = this;
-  return new Promise(function (resolve, reject) {
+  return new Promise(function(resolve, reject) {
     var key = _this.config.key
     var jwtClient = new googleapis.auth.JWT(key.client_email, null, key.private_key, ['https://www.googleapis.com/auth/drive'], null);
 
@@ -26,7 +31,10 @@ ghostGoogleDrive.prototype.save = function(file){
         return;
       }
 
-      var drive = googleapis.drive({ version: 'v2', auth: jwtClient });
+      var drive = googleapis.drive({
+        version: 'v2',
+        auth: jwtClient
+      });
       drive.files.insert({
         resource: {
           title: file.name,
@@ -36,24 +44,24 @@ ghostGoogleDrive.prototype.save = function(file){
           mimeType: file.type,
           body: fs.createReadStream(file.path)
         }
-      }, function(err,data){
-        if(err){
+      }, function(err, data) {
+        if (err) {
           console.log(err)
           reject(err)
           return;
         }
         // make the url looks like a file
-        resolve('/content/images/'+data.id+'.'+data.fileExtension);
+        resolve('/content/images/' + data.id + '.' + data.fileExtension);
       });
     });
-  }); 
+  });
 };
 
-ghostGoogleDrive.prototype.serve = function(){
+ghostGoogleDrive.prototype.serve = function() {
   var _this = this;
-  return function (req, res, next) {
-   // get the file id from url
-    var id = req.path.replace('/','').split('.')[0];
+  return function(req, res, next) {
+    // get the file id from url
+    var id = req.path.replace('/', '').split('.')[0];
 
     var key = _this.config.key
     var jwtClient = new googleapis.auth.JWT(key.client_email, null, key.private_key, ['https://www.googleapis.com/auth/drive'], null);
@@ -63,13 +71,18 @@ ghostGoogleDrive.prototype.serve = function(){
         console.log(err);
         next()
       }
-      var drive = googleapis.drive({ version: 'v2', auth: jwtClient });
-      drive.files.get({fileId:id}, function(err, file){
-        if(!err) {
-          var newReq = https.request(file.downloadUrl+'&access_token='+tokens.access_token, function(newRes) {
+      var drive = googleapis.drive({
+        version: 'v2',
+        auth: jwtClient
+      });
+      drive.files.get({
+        fileId: id
+      }, function(err, file) {
+        if (!err) {
+          var newReq = https.request(file.downloadUrl + '&access_token=' + tokens.access_token, function(newRes) {
             // Modify google headers here to cache!
             var headers = newRes.headers;
-            headers['content-disposition'] = "attachment; filename="+file.originalFilename;
+            headers['content-disposition'] = "attachment; filename=" + file.originalFilename;
             headers['cache-control'] = 'public, max-age=1209600';
             delete headers['expires'];
             res.writeHead(newRes.statusCode, headers);
@@ -89,5 +102,12 @@ ghostGoogleDrive.prototype.serve = function(){
   };
 };
 
+ghostGoogleDrive.prototype.exists = function (filename) {
+    return false;
+};
+
+ghostGoogleDrive.prototype.delete = function (fileName, targetDir) {
+    return true;
+};
 
 module.exports = ghostGoogleDrive
